82be1520022802645fafc38d8e263018a12d13ac
Alina Dang <alina.dang@duke.edu>
2025-10-07 15:43:17 -0400

    added second endpointfor wishlist and tested with Restman

--- app/wishlist.py
+++ app/wishlist.py
@@ -12,3 +12,10 @@ def wishlist():
         return jsonify({}), 404
     items = WishlistItem.get_all_by_uid(current_user.id)
     return jsonify([item.__dict__ for item in items])
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+@login_required
+def wishlist_add(product_id):
+   """Add a product to the current user's wishlist and redirect."""
+   WishlistItem.add(current_user.id, product_id)
+   return redirect(url_for('wishlist.wishlist'))

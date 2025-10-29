bdafc2c90eff873f17fd590a9159561732c99984
Alyssa Yang <sy323@duke.edu>
2025-10-28 20:28:53 -0400

    updated sellers list

--- app/index.py
+++ app/index.py
@@ -11,8 +11,9 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    # get all available products for sale:
-    products = Product.get_all(True)
+    # don't show any products on home page - users must click "Browse Products"
+    products = []
+    
     # find the products current user has bought:
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_uid_since(
